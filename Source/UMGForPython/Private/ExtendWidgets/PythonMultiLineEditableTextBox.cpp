f45002bd84d2b102bbc64c5b244b337273c7b830
SKY123C <289079202@qq.com>
2024-09-19 15:21:35 +0800

    add editable text box richtext

--- Source/UMGForPython/Private/ExtendWidgets/PythonMultiLineEditableTextBox.cpp
+++ Source/UMGForPython/Private/ExtendWidgets/PythonMultiLineEditableTextBox.cpp
@@ -1,11 +1,157 @@
 ﻿#include "ExtendWidgets/PythonMultiLineEditableTextBox.h"
+#include "DataTableEditorUtils.h"
+#include "Components/RichTextBlock.h"
+#include "Framework/Text/RichTextMarkupProcessing.h"
+#include "Framework/Text/ITextDecorator.h"
+#include "Framework/Text/SlateTextRun.h"
 
-TSharedRef<SWidget> PythonMultiLineEditableTextBox::RebuildWidget()
+
+class FPythonRichTextDecorator : public ITextDecorator
 {
-	MyEditableTextBlock = SNew(SMultiLineEditableTextBox);
+public:
+	FPythonRichTextDecorator(const FTextBlockStyle& TextStyle);
+
+	virtual ~FPythonRichTextDecorator(){};
+
+	virtual bool Supports(const FTextRunParseResults& RunParseResult, const FString& Text) const override;
+
+	virtual TSharedRef<ISlateRun> Create(const TSharedRef<FTextLayout>& TextLayout, const FTextRunParseResults& RunParseResult, const FString& OriginalText, const TSharedRef<FString>& InOutModelText, const ISlateStyle* Style) override;
+	
+	void ExplodeRunInfo(const FRunInfo& InRunInfo, uint16& OutFontSize, FLinearColor& OutFontColor) const;
+
+	FTextBlockStyle CreateTextBlockStyle(const FRunInfo& InRunInfo);
+
+protected:
+	FTextBlockStyle DefaultStyle;
+};
+
+FPythonRichTextDecorator::FPythonRichTextDecorator(const FTextBlockStyle& TextStyle)
+	:DefaultStyle(TextStyle)
+{
+}
+
+
+bool FPythonRichTextDecorator::Supports(const FTextRunParseResults& RunParseResult, const FString& Text) const
+{
+	return (RunParseResult.Name == TEXT("PythonRichText"));
+}
+
+TSharedRef<ISlateRun> FPythonRichTextDecorator::Create(const TSharedRef<FTextLayout>& TextLayout, const FTextRunParseResults& RunParseResult, const FString& OriginalText, const TSharedRef<FString>& InOutModelText, const ISlateStyle* Style)
+{
+	FRunInfo RunInfo(RunParseResult.Name);
+	for (const TPair<FString, FTextRange>& Pair : RunParseResult.MetaData)
+	{
+		RunInfo.MetaData.Add(Pair.Key, OriginalText.Mid(Pair.Value.BeginIndex, Pair.Value.EndIndex - Pair.Value.BeginIndex));
+	}
+	FTextRange ModelRange;
+	ModelRange.BeginIndex = InOutModelText->Len();
+	*InOutModelText += OriginalText.Mid(RunParseResult.ContentRange.BeginIndex, RunParseResult.ContentRange.EndIndex - RunParseResult.ContentRange.BeginIndex);
+	ModelRange.EndIndex = InOutModelText->Len();
+
+
+	return FSlateTextRun::Create(RunInfo, InOutModelText, CreateTextBlockStyle(RunInfo), ModelRange);
+}
+
+void FPythonRichTextDecorator::ExplodeRunInfo(const FRunInfo& InRunInfo, uint16& OutFontSize, FLinearColor& OutFontColor) const
+{
+	const FString* const FontSizeString = InRunInfo.MetaData.Find(TEXT("FontSize"));
+	OutFontSize = 10;
+	if (FontSizeString)
+	{
+		OutFontSize = static_cast<uint16>(FPlatformString::Atoi(**FontSizeString));
+	}
+
+	OutFontColor = FLinearColor::White;
+	const FString* const FontColorString = InRunInfo.MetaData.Find(TEXT("FontColor"));
+	if (FontColorString && !OutFontColor.InitFromString(*FontColorString))
+	{
+		OutFontColor = FLinearColor::White;
+	}
+}
+
+FTextBlockStyle FPythonRichTextDecorator::CreateTextBlockStyle(const FRunInfo& InRunInfo)
+{
+	uint16 FontSize;
+	FLinearColor FontColor;
+	ExplodeRunInfo(InRunInfo, FontSize, FontColor);
+	FTextBlockStyle TextBlockStyle;
+	TextBlockStyle.SetFont(FCoreStyle::GetDefaultFontStyle("Regular", FontSize));
+	TextBlockStyle.SetColorAndOpacity(FontColor);
+	return TextBlockStyle;
+}
+
+UPythonMultiLineEditableTextBox::UPythonMultiLineEditableTextBox()
+{
+}
+
+void UPythonMultiLineEditableTextBox::SetText(FText InText)
+{
+	if (MyEditableTextBlock.IsValid())
+	{
+		MyEditableTextBlock->SetText(InText);
+	}
+	else
+	{
+		Text = InText;
+	}
+}
+
+FText UPythonMultiLineEditableTextBox::GetText()
+{
+	FText OutText;
+	if (MyEditableTextBlock.IsValid())
+	{
+		OutText = MyEditableTextBlock->GetText();
+	}
+	return OutText;
+}
+
+TSharedRef<SWidget> UPythonMultiLineEditableTextBox::RebuildWidget()
+{
+	StyleInstance = MakeShareable(new FSlateStyleSet("RichTextStyle"));
+	StyleInstance->Set("PythonRichText", TextBlockStyle);
+	TArray< TSharedRef< class ITextDecorator > > Decorators;
+	Decorators.Add(MakeShareable(new FPythonRichTextDecorator(TextBlockStyle)));
+	MessagesTextMarshaller = FRichTextLayoutMarshaller::Create(
+		Decorators,
+		StyleInstance.Get()
+	);
+	MyEditableTextBlock = SNew(SMultiLineEditableTextBox)
+							.Marshaller(MessagesTextMarshaller)
+							.AutoWrapText(true)
+							.AlwaysShowScrollbars(true)
+							.Text(Text)
+							.IsReadOnly(ReadOnly);
 	return MyEditableTextBlock.ToSharedRef();
 }
 
-void PythonMultiLineEditableTextBox::SynchronizeProperties()
+void UPythonMultiLineEditableTextBox::SynchronizeProperties()
 {
+	Super::SynchronizeProperties();
+	if (MyEditableTextBlock.IsValid())
+	{
+		MyEditableTextBlock->SetText(Text);
+		MyEditableTextBlock->Refresh();
+	}
+}
+
+TSharedRef<FPythonTextLayoutMarshaller> FPythonTextLayoutMarshaller::Create(TArray<TSharedPtr<FPythonTextMessage>> InMessages)
+{
+	return MakeShareable(new FPythonTextLayoutMarshaller(MoveTemp(InMessages)));
+}
+
+void FPythonTextLayoutMarshaller::SetText(const FString& SourceString, FTextLayout& TargetTextLayout)
+{
+	TextLayout = &TargetTextLayout;
+}
+
+void FPythonTextLayoutMarshaller::GetText(FString& TargetString, const FTextLayout& SourceTextLayout)
+{
+	SourceTextLayout.GetAsText(TargetString);
+}
+
+FPythonTextLayoutMarshaller::FPythonTextLayoutMarshaller(TArray<TSharedPtr<FPythonTextMessage>> InMessages)
+{
+	Messages = MoveTemp(InMessages);
+	StyleSet = &FAppStyle::Get();
 }
--- Source/UMGForPython/Public/ExtendWidgets/PythonMultiLineEditableTextBox.h
+++ Source/UMGForPython/Public/ExtendWidgets/PythonMultiLineEditableTextBox.h
@@ -6,27 +6,71 @@
 #include "Components/Widget.h"
 #include "UMG.h"
 #include "Framework/Text/BaseTextLayoutMarshaller.h"
+#include "Framework/Text/RichTextLayoutMarshaller.h"
 #include "PythonMultiLineEditableTextBox.generated.h"
+
+
+struct FPythonTextMessage
+{
+	TSharedRef<FString> Message;
+	FTextBlockStyle TextStyle;
+	FPythonTextMessage(const TSharedRef<FString>& NewMessage, FTextBlockStyle& Style)
+		: Message(NewMessage)
+	{
+		TextStyle.Font.Size = 5;
+	}
+};
 
 class FPythonTextLayoutMarshaller : public FBaseTextLayoutMarshaller
 {
 
 public:
+	static TSharedRef< FPythonTextLayoutMarshaller > Create(TArray<TSharedPtr<FPythonTextMessage>> InMessages);
+	virtual ~FPythonTextLayoutMarshaller(){};
 
+	// ITextLayoutMarshaller
+	virtual void SetText(const FString& SourceString, FTextLayout& TargetTextLayout) override;
+	virtual void GetText(FString& TargetString, const FTextLayout& SourceTextLayout) override;
+
+public:
+	FPythonTextLayoutMarshaller(TArray< TSharedPtr<FPythonTextMessage> > InMessages);
+
+protected:
+	const ISlateStyle* StyleSet;
+	FTextLayout* TextLayout;
+	TArray< TSharedPtr<FPythonTextMessage> > Messages;
 };
 
 
 UCLASS()
-class PythonMultiLineEditableTextBox : public UWidget
+class UPythonMultiLineEditableTextBox : public UWidget
 {
 	GENERATED_BODY()
 
 public:
-	PythonMultiLineEditableTextBox();
+	UPythonMultiLineEditableTextBox();
+
+public:
+	UPROPERTY()
+	FText Text;
+	UFUNCTION(BlueprintCallable)
+	void SetText(FText InText);
+
+	UFUNCTION(BlueprintCallable)
+	FText GetText();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTextBlockStyle TextBlockStyle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool ReadOnly;
+
 protected:
 	virtual TSharedRef<SWidget> RebuildWidget() override final;
 	virtual void SynchronizeProperties();
 
 protected:
+	TSharedPtr< FRichTextLayoutMarshaller > MessagesTextMarshaller;
 	TSharedPtr<SMultiLineEditableTextBox> MyEditableTextBlock;
+	TSharedPtr<class FSlateStyleSet> StyleInstance;
 };
